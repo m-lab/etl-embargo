56a538dddae842e24bc44d11255e35588da38a56
yachang <yachang@users.noreply.github.com>
2017-04-03 14:22:11 -0400

    Add files via upload

---
+++ embargo.go
@@ -0,0 +1,171 @@
+// Embargo implementation.
+package embargo
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/net/context"
+	storage "google.golang.org/api/storage/v1"
+)
+
+var (
+	sourceBucket      = ""
+	destPrivateBucket = ""
+	destPublicBucket  = ""
+	embargoCheck      = new(EmbargoCheck)
+	embargoDate       = 0
+)
+
+// Write results to GCS.
+func WriteResults(tarfileName string, service *storage.Service, privateBuf, publicBuf bytes.Buffer) bool {
+	privateTarfileName := strings.Replace(tarfileName, ".tgz", "-e.tgz", -1)
+	publicObject := &storage.Object{Name: tarfileName}
+	privateObject := &storage.Object{Name: privateTarfileName}
+	if _, err := service.Objects.Insert(destPublicBucket, publicObject).Media(&publicBuf).Do(); err != nil {
+		fmt.Printf("Objects insert failed: %v\n", err)
+		return false
+	}
+
+	if _, err := service.Objects.Insert(destPrivateBucket, privateObject).Media(&privateBuf).Do(); err != nil {
+		fmt.Printf("Objects insert failed: %v\n", err)
+		return false
+	}
+	return true
+}
+
+// Split one tar files into 2 buffers.
+func SplitFile(content io.Reader) (bool, bytes.Buffer, bytes.Buffer) {
+	var privateBuf bytes.Buffer
+	var publicBuf bytes.Buffer
+	embargoCheck.ReadWhitelistFromGCS("whitelist")
+	embargoCheck.Embargodate = embargoDate
+	// Create tar reader
+	zipReader, err := gzip.NewReader(content)
+	if err != nil {
+		fmt.Println(err)
+		return false, privateBuf, publicBuf
+	}
+	defer zipReader.Close()
+	unzippedImage, err := ioutil.ReadAll(zipReader)
+	if err != nil {
+		fmt.Println(err)
+		return false, privateBuf, publicBuf
+	}
+	unzippedReader := bytes.NewReader(unzippedImage)
+	tarReader := tar.NewReader(unzippedReader)
+
+	privateGzw := gzip.NewWriter(&privateBuf)
+	publicGzw := gzip.NewWriter(&publicBuf)
+	privateTw := tar.NewWriter(privateGzw)
+	publicTw := tar.NewWriter(publicGzw)
+
+	// Handle one tar file
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+			return false, privateBuf, publicBuf
+		}
+		basename := filepath.Base(header.Name)
+		info := header.FileInfo()
+		hdr := new(tar.Header)
+		hdr.Name = header.Name
+		hdr.Size = info.Size()
+		hdr.Mode = int64(info.Mode())
+		hdr.ModTime = info.ModTime()
+		output, err := ioutil.ReadAll(tarReader)
+		if strings.Contains(basename, "web100") && embargoCheck.ShouldEmbargo(basename) {
+			// put this file to a private buffer
+			if err := privateTw.WriteHeader(hdr); err != nil {
+				fmt.Println(err)
+				return false, privateBuf, publicBuf
+			}
+			if _, err := privateTw.Write([]byte(output)); err != nil {
+				fmt.Println(err)
+				return false, privateBuf, publicBuf
+			}
+		} else {
+			// put this file to a public buffer
+			if err := publicTw.WriteHeader(hdr); err != nil {
+				fmt.Println(err)
+			}
+			if _, err := publicTw.Write([]byte(output)); err != nil {
+				fmt.Println(err)
+				return false, privateBuf, publicBuf
+			}
+		}
+	}
+
+	if err := publicTw.Close(); err != nil {
+		fmt.Println(err)
+		return false, privateBuf, publicBuf
+	}
+	if err := privateTw.Close(); err != nil {
+		fmt.Println(err)
+		return false, privateBuf, publicBuf
+	}
+	if err := publicGzw.Close(); err != nil {
+		fmt.Println(err)
+		return false, privateBuf, publicBuf
+	}
+	if err := privateGzw.Close(); err != nil {
+		fmt.Println(err)
+		return false, privateBuf, publicBuf
+	}
+	return true, privateBuf, publicBuf
+}
+
+// EmbargoOneTar process one tar file, split it to 2 files, the embargoed files
+// will be saved in a private dir, and the unembargoed part will be save in a
+// public dir.
+// The private file will have a different name, so it can be copied to public
+// bucket directly when it becomes one year old.
+func EmbargoOneTar(content io.Reader, tarfileName string, service *storage.Service) bool {
+	suc, privateBuf, publicBuf := SplitFile(content)
+	if suc && WriteResults(tarfileName, service, privateBuf, publicBuf) {
+		return true
+	}
+	return false
+}
+
+// Embargo do embargo ckecking to all files in the sourceBucket.
+func Embargo() bool {
+	embargoService := CreateService()
+	if embargoService == nil {
+		fmt.Printf("Storage service was not initialized.\n")
+		return false
+	}
+
+	sourceFiles := embargoService.Objects.List(sourceBucket)
+	sourceFilesList, err := sourceFiles.Context(context.Background()).Do()
+	if err != nil {
+		fmt.Printf("Objects List of source bucket failed: %v\n", err)
+		return false
+	}
+	for _, oneItem := range sourceFilesList.Items {
+		//fmt.Printf(oneItem.Name + "\n")
+		if !strings.Contains(oneItem.Name, "tgz") || !strings.Contains(oneItem.Name, "sidestream") {
+			continue
+		}
+
+		fileContent, err := embargoService.Objects.Get(sourceBucket, oneItem.Name).Download()
+		if err != nil {
+			fmt.Println(err)
+			return false
+		}
+		if !EmbargoOneTar(fileContent.Body, oneItem.Name, embargoService) {
+			return false
+		}
+	}
+	return true
+}
---
+++ embargo_test.go
@@ -0,0 +1,55 @@
+package embargo
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+/*
+// End to end test, requires authentication.
+func TestEmbargo(t *testing.T) {
+	embargoDate = 20160315
+	sourceBucket = "sidestream-embargo"
+	destPublicBucket = "embargo-output"
+	destPrivateBucket = "mlab-embargoed-data"
+	if !Embargo() {
+		t.Error("Did not perform embargo ocrrectly.\n")
+	}
+	return
+}
+*/
+
+func TestSplitTarFile(t *testing.T) {
+	// Load input tar file.
+	file, err := os.Open("testdata/20170315T000000Z-mlab3-sea03-sidestream-0000.tgz")
+	if err != nil {
+		t.Fatal("cannot opene test data.")
+	}
+	defer file.Close()
+
+	suc, privateBuf, publicBuf := SplitFile(file)
+	if !suc {
+		t.Error("Did not perform embargo ocrrectly.\n")
+	}
+	publicGolden, err := os.Open("testdata/20170315T000000Z-mlab3-sea03-sidestream-0000-p.tgz")
+	if err != nil {
+		t.Fatal("cannot opene public golden data.")
+	}
+	defer publicGolden.Close()
+	publicContent, err := ioutil.ReadAll(publicGolden)
+	if !bytes.Equal(publicBuf.Bytes(), publicContent) {
+		t.Error("Public data not correct.\n")
+	}
+
+	privateGolden, err := os.Open("testdata/20170315T000000Z-mlab3-sea03-sidestream-0000-e.tgz")
+	if err != nil {
+		t.Fatal("cannot opene private golden data.")
+	}
+	defer privateGolden.Close()
+	privateContent, err := ioutil.ReadAll(privateGolden)
+	if !bytes.Equal(privateBuf.Bytes(), privateContent) {
+		t.Error("Public data not correct.\n")
+	}
+}

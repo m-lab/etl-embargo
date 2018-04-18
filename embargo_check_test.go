0d223f1995788786becff01e8fd8022655521923
yachang <yachang@users.noreply.github.com>
2018-04-18 12:30:16 -0400

    Update embargo_check_test.go

--- embargo_check_test.go
+++ embargo_check_test.go
@@ -31,39 +31,26 @@ func TestReadWhitelistFromLocal(t *testing.T) {
 	return
 }
 
-/*
-// Require authentication to run.
-func TestReadWhitelistFromGCS(t *testing.T) {
-	embargo_check := new(EmbargoCheck)
-	embargo_check.ReadWhitelistFromGCS("whitelist")
-	if !embargo_check.Whitelist["213.244.128.170"] {
-		t.Error("missing IP in Whitelist: want '213.244.128.170'\n")
+func TestParseJson(t *testing.T) {
+	body := []byte(`[
+  {
+    "hostname": "mlab2.acc02.measurement-lab.org", 
+    "ipv4": "196.49.14.214", 
+    "ipv6": ""
+  }, 
+  {
+    "hostname": "mlab3.acc02.measurement-lab.org", 
+    "ipv4": "196.49.14.227", 
+    "ipv6": ""
+  }, 
+  {
+    "hostname": "mlab1.acc02.measurement-lab.org", 
+    "ipv4": "196.49.14.201", 
+    "ipv6": ""
+  }
+]`)
+	whitelist, err := embargo.ParseJson(body)
+	if err != nil || len(whitelist) != 3 || !whitelist["196.49.14.227"] {
+		t.Error("Do not parse bytes into struct correctly.")
 	}
-	if embargo_check.Whitelist["2001:4c08:2003:2::16"] {
-		t.Error("IP 2001:4c08:2003:2::16 should not be in Whitelist.\n")
-	}
-	return
-}
-*/
-func TestShouldEmbargo(t *testing.T) {
-	embargo_check := new(embargo.EmbargoCheck)
-	embargo_check.ReadWhitelistFromLocal("testdata/whitelist")
-	// After embargo date and IP not whitelisted. Return true, embargoed.
-	// TODO: ShouldEmbargo should allow injecting a timestamp to check.
-	filename1 := "20180225T23:00:00Z_4.34.58.34_0.web100.gz"
-	if !embargo_check.ShouldEmbargo(filename1) {
-		t.Errorf("ShouldEmbargo(%s) = false, but file date is after embargo date and IP not whitelisted (%v).\n", filename1, embargo_check.Whitelist)
-	}
-
-	// After embargo date and IP whitelisted. Return false, not embargoed.
-	filename2 := "20170225T23:00:00Z_213.244.128.170_0.web100.gz"
-	if embargo_check.ShouldEmbargo(filename2) {
-		t.Errorf("ShouldEmbargo(%s) = true, but after embargo date and IP whitelisted (%v).\n", filename2, embargo_check.Whitelist)
-	}
-	// Before embargo date. Return false, not embargoed
-	filename3 := "20150225T23:00:00Z_213.244.128.1_0.web100.gz"
-	if embargo_check.ShouldEmbargo(filename3) {
-		t.Errorf("ShouldEmbargo(%s) = true, but before embargo date.\n", filename3)
-	}
-	return
 }
